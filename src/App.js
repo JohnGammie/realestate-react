97d2f8c4a11afe7e770065b8b03fc4102e8ea43c
JohnGammie <gammie@y7mail.com>
2022-03-12 17:07:16 +1100

    Add basic 404 page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Rent from "./pages/rent";
 import SignIn from "./pages/sign-in";
 import Join from "./pages/join";
 import "./styles.css";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
+          <Route path="*" element={<NotFound />}></Route>
           <Route path="/" element={<Home />}></Route>
           <Route path="/buy" element={<Buy />}></Route>
           <Route path="/rent" element={<Rent />}></Route>
---
+++ src/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div className="notFound">
+      <p>404 Resource Not Found</p>
+      <a href="/">return to home</a>
+    </div>
+  );
+};
+
+export default NotFound;
